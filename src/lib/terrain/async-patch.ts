7e51d25b87aaaeff5a68a189adb8515c12cf6724
Jérémie Piellard <jeremie.piellard@gmail.com>
2024-06-27 19:55:45 +0200

    refactor: simplify AsyncPatch.dispose()

--- src/lib/terrain/async-patch.ts
+++ src/lib/terrain/async-patch.ts
@@ -87,7 +87,7 @@ class AsyncPatch {
         return this.invisibilityTimestamp;
     }
 
-    public async dispose(): Promise<void> {
+    public dispose(): void {
         if (!this.data.disposed) {
             this.data.disposed = true;
             this.patch?.dispose();
