850eb6978689b359d329c5fc9ccf99c206cac76b
Jérémie Piellard <jeremie.piellard@gmail.com>
2024-07-05 15:22:30 +0200

    build: export VoxelmapVisibilityComputer

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,14 +2,14 @@ export { ELogLevel, setVerbosity } from './helpers/logger';
 export type { IHeightmap, IHeightmapCoords, IHeightmapSample } from './terrain/heightmap/i-heightmap';
 export { TerrainViewer } from './terrain/terrain-viewer';
 export type { ILocalMapData, IVoxelMap, IVoxelMaterial, VoxelsChunkSize } from './terrain/voxelmap/i-voxelmap';
-
+export {
+    VoxelmapViewerAutonomous,
+    type VoxelmapViewerAutonomousOptions,
+} from './terrain/voxelmap/viewer/autonomous/voxelmap-viewer-autonomous';
 export {
     VoxelmapViewer,
     type ComputationStatus,
     type VoxelmapViewerOptions,
     type VoxelsChunkData,
 } from './terrain/voxelmap/viewer/simple/voxelmap-viewer';
-export {
-    VoxelmapViewerAutonomous,
-    type VoxelmapViewerAutonomousOptions,
-} from './terrain/voxelmap/viewer/autonomous/voxelmap-viewer-autonomous';
+export { VoxelmapVisibilityComputer } from './terrain/voxelmap/voxelmap-visibility-computer';
