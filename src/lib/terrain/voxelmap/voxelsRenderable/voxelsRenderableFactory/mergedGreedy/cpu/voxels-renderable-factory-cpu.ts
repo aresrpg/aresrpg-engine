50e5fe848a79a2dc10960bd8d88ce2ec998a47d2
Jérémie Piellard <jeremie.piellard@gmail.com>
2024-07-09 23:30:29 +0200

    refactor: simplification

--- src/lib/terrain/voxelmap/voxelsRenderable/voxelsRenderableFactory/mergedGreedy/cpu/voxels-renderable-factory-cpu.ts
+++ src/lib/terrain/voxelmap/voxelsRenderable/voxelsRenderableFactory/mergedGreedy/cpu/voxels-renderable-factory-cpu.ts
@@ -12,7 +12,8 @@ type FaceData = {
 };
 
 type VoxelsChunkCache = VoxelsChunkData & {
-    neighbourExists(voxelIndex: number, neighbourRelativePosition: THREE.Vector3): boolean;
+    buildIndex(position: THREE.Vector3Like): number;
+    neighbourExists(voxelIndex: number, neighbourRelativePosition: THREE.Vector3Like): boolean;
 };
 
 class VoxelsRenderableFactoryCpu extends VoxelsRenderableFactory {
@@ -46,29 +47,88 @@ class VoxelsRenderableFactoryCpu extends VoxelsRenderableFactory {
             let faceId = 0;
             const faceVerticesData = new Uint32Array(uint32PerVertex * 4);
             const voxelsChunkCache = this.buildLocalMapCache(voxelsChunkData);
-            for (const faceData of this.iterateOnVisibleFacesWithCache(voxelsChunkCache)) {
-                const faceNoiseId = faceId++ % this.vertexData2Encoder.faceNoiseId.maxValue;
 
-                faceData.verticesData.forEach((faceVertexData: VertexData, faceVertexIndex: number) => {
-                    faceVerticesData[2 * faceVertexIndex + 0] = this.vertexData1Encoder.encode(
-                        faceData.voxelLocalPosition,
-                        faceVertexData.localPosition,
-                        faceData.faceId,
-                        faceVertexData.ao,
-                        [faceVertexData.roundnessX, faceVertexData.roundnessY]
-                    );
-                    faceVerticesData[2 * faceVertexIndex + 1] = this.vertexData2Encoder.encode(
-                        faceData.voxelMaterialId,
-                        faceNoiseId,
-                        this.cube.faces[faceData.faceType].normal.id,
-                        this.cube.faces[faceData.faceType].uvRight.id
-                    );
-                });
+            const localPosition = { x: 0, y: 0, z: 0 };
+            for (localPosition.z = 1; localPosition.z < voxelsChunkCache.size.z - 1; localPosition.z++) {
+                for (localPosition.y = 1; localPosition.y < voxelsChunkCache.size.y - 1; localPosition.y++) {
+                    for (localPosition.x = 1; localPosition.x < voxelsChunkCache.size.x - 1; localPosition.x++) {
+                        const cacheIndex = voxelsChunkCache.buildIndex(localPosition);
+                        const cacheData = voxelsChunkCache.data[cacheIndex];
+                        if (typeof cacheData === 'undefined') {
+                            throw new Error();
+                        }
+                        if (cacheData === 0) {
+                            continue; // no voxel here
+                        }
 
-                for (const index of this.cube.faceIndices) {
-                    const vertexIndex = uint32PerVertex * bufferData.verticesCount++;
-                    bufferData.buffer[vertexIndex] = faceVerticesData[2 * index + 0]!;
-                    bufferData.buffer[vertexIndex + 1] = faceVerticesData[2 * index + 1]!;
+                        const voxelLocalPosition = { x: localPosition.x - 1, y: localPosition.y - 1, z: localPosition.z - 1 };
+                        const voxelMaterialId = cacheData - 1;
+
+                        for (const face of Object.values(this.cube.faces)) {
+                            if (voxelsChunkCache.neighbourExists(cacheIndex, face.normal.vec)) {
+                                // this face will be hidden -> skip it
+                                continue;
+                            }
+
+                            const faceData = {
+                                voxelLocalPosition,
+                                voxelMaterialId,
+                                faceType: face.type,
+                                faceId: face.id,
+                                verticesData: face.vertices.map((faceVertex: Cube.FaceVertex): VertexData => {
+                                    let ao = 0;
+                                    const [a, b, c] = faceVertex.shadowingNeighbourVoxels.map(neighbourVoxel =>
+                                        voxelsChunkCache.neighbourExists(cacheIndex, neighbourVoxel)
+                                    ) as [boolean, boolean, boolean];
+                                    if (a && b) {
+                                        ao = 3;
+                                    } else {
+                                        ao = +a + +b + +c;
+                                    }
+
+                                    let roundnessX = true;
+                                    let roundnessY = true;
+                                    for (const neighbourVoxel of faceVertex.edgeNeighbourVoxels.x) {
+                                        roundnessX &&= !voxelsChunkCache.neighbourExists(cacheIndex, neighbourVoxel);
+                                    }
+                                    for (const neighbourVoxel of faceVertex.edgeNeighbourVoxels.y) {
+                                        roundnessY &&= !voxelsChunkCache.neighbourExists(cacheIndex, neighbourVoxel);
+                                    }
+
+                                    return {
+                                        localPosition: faceVertex.vertex,
+                                        ao,
+                                        roundnessX,
+                                        roundnessY,
+                                    };
+                                }) as [VertexData, VertexData, VertexData, VertexData],
+                            };
+
+                            const faceNoiseId = faceId++ % this.vertexData2Encoder.faceNoiseId.maxValue;
+
+                            faceData.verticesData.forEach((faceVertexData: VertexData, faceVertexIndex: number) => {
+                                faceVerticesData[2 * faceVertexIndex + 0] = this.vertexData1Encoder.encode(
+                                    faceData.voxelLocalPosition,
+                                    faceVertexData.localPosition,
+                                    faceData.faceId,
+                                    faceVertexData.ao,
+                                    [faceVertexData.roundnessX, faceVertexData.roundnessY]
+                                );
+                                faceVerticesData[2 * faceVertexIndex + 1] = this.vertexData2Encoder.encode(
+                                    faceData.voxelMaterialId,
+                                    faceNoiseId,
+                                    this.cube.faces[faceData.faceType].normal.id,
+                                    this.cube.faces[faceData.faceType].uvRight.id
+                                );
+                            });
+
+                            for (const index of this.cube.faceIndices) {
+                                const vertexIndex = uint32PerVertex * bufferData.verticesCount++;
+                                bufferData.buffer[vertexIndex] = faceVerticesData[2 * index + 0]!;
+                                bufferData.buffer[vertexIndex + 1] = faceVerticesData[2 * index + 1]!;
+                            }
+                        }
+                    }
                 }
             }
 
@@ -93,84 +153,9 @@ class VoxelsRenderableFactoryCpu extends VoxelsRenderableFactory {
             };
 
             return Object.assign(voxelsChunkData, {
+                buildIndex: buildIndexUnsafe,
                 neighbourExists,
             });
-        },
-
-        *iterateOnVisibleFacesWithCache(voxelsChunkCache: VoxelsChunkCache): Generator<FaceData> {
-            if (voxelsChunkCache.isEmpty) {
-                return;
-            }
-
-            let cacheIndex = 0;
-            const localPosition = { x: 0, y: 0, z: 0 };
-            for (localPosition.z = 0; localPosition.z < voxelsChunkCache.size.z; localPosition.z++) {
-                for (localPosition.y = 0; localPosition.y < voxelsChunkCache.size.y; localPosition.y++) {
-                    for (localPosition.x = 0; localPosition.x < voxelsChunkCache.size.x; localPosition.x++) {
-                        const cacheData = voxelsChunkCache.data[cacheIndex];
-                        if (typeof cacheData === 'undefined') {
-                            throw new Error();
-                        }
-
-                        if (cacheData > 0) {
-                            // if there is a voxel there
-                            if (
-                                localPosition.x > 0 &&
-                                localPosition.y > 0 &&
-                                localPosition.z > 0 &&
-                                localPosition.x < voxelsChunkCache.size.x - 1 &&
-                                localPosition.y < voxelsChunkCache.size.y - 1 &&
-                                localPosition.z < voxelsChunkCache.size.z - 1
-                            ) {
-                                const voxelLocalPosition = { x: localPosition.x - 1, y: localPosition.y - 1, z: localPosition.z - 1 };
-                                const voxelMaterialId = cacheData - 1;
-
-                                for (const face of Object.values(this.cube.faces)) {
-                                    if (voxelsChunkCache.neighbourExists(cacheIndex, face.normal.vec)) {
-                                        // this face will be hidden -> skip it
-                                        continue;
-                                    }
-
-                                    yield {
-                                        voxelLocalPosition,
-                                        voxelMaterialId,
-                                        faceType: face.type,
-                                        faceId: face.id,
-                                        verticesData: face.vertices.map((faceVertex: Cube.FaceVertex): VertexData => {
-                                            let ao = 0;
-                                            const [a, b, c] = faceVertex.shadowingNeighbourVoxels.map(neighbourVoxel =>
-                                                voxelsChunkCache.neighbourExists(cacheIndex, neighbourVoxel)
-                                            ) as [boolean, boolean, boolean];
-                                            if (a && b) {
-                                                ao = 3;
-                                            } else {
-                                                ao = +a + +b + +c;
-                                            }
-
-                                            let roundnessX = true;
-                                            let roundnessY = true;
-                                            for (const neighbourVoxel of faceVertex.edgeNeighbourVoxels.x) {
-                                                roundnessX &&= !voxelsChunkCache.neighbourExists(cacheIndex, neighbourVoxel);
-                                            }
-                                            for (const neighbourVoxel of faceVertex.edgeNeighbourVoxels.y) {
-                                                roundnessY &&= !voxelsChunkCache.neighbourExists(cacheIndex, neighbourVoxel);
-                                            }
-
-                                            return {
-                                                localPosition: faceVertex.vertex,
-                                                ao,
-                                                roundnessX,
-                                                roundnessY,
-                                            };
-                                        }) as [VertexData, VertexData, VertexData, VertexData],
-                                    };
-                                }
-                            }
-                        }
-                        cacheIndex++;
-                    }
-                }
-            }
         },
     };
 
@@ -193,7 +178,6 @@ class VoxelsRenderableFactoryCpu extends VoxelsRenderableFactory {
     vertexData2Encoder: ${this.serializableFactory.vertexData2Encoder.serialize()},
     ${this.serializableFactory.buildBuffer},
     ${this.serializableFactory.buildLocalMapCache},
-    ${this.serializableFactory.iterateOnVisibleFacesWithCache},
 }`;
     }
 }
