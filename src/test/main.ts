ff1967794f72e9b50d77f0af80f2a88b9f15d63a
Jérémie Piellard <jeremie.piellard@gmail.com>
2024-07-10 17:08:12 +0200

    test: new cacheless test map

--- src/test/main.ts
+++ src/test/main.ts
@@ -3,10 +3,15 @@ import { ELogLevel, setVerbosity } from '../lib/index';
 import { TestTerrain } from './test-terrain';
 import { TestTerrainAutonomous } from './test-terrain-autonomous';
 import { VoxelMap } from './voxel-map';
+import { VoxelMapCacheless } from './voxel-map-cacheless';
 
 setVerbosity(ELogLevel.WARN);
 
-const voxelMap = new VoxelMap(2048, 2048, 200, 64, 'fixed_seed');
+const mapScaleXZ = 200;
+const mapScaleY = 64;
+const mapSeed = 'fixed_seed';
+
+const voxelMap = new VoxelMapCacheless(mapScaleXZ, mapScaleY, mapSeed) || new VoxelMap(2048, 2048, mapScaleXZ, mapScaleY, mapSeed);
 
 const testNewTerrain = true;
 const testScene = testNewTerrain ? new TestTerrain(voxelMap) : new TestTerrainAutonomous(voxelMap);
--- src/test/test-terrain.ts
+++ src/test/test-terrain.ts
@@ -46,6 +46,7 @@ class TestTerrain extends TestBase {
         this.terrainViewer = new TerrainViewer(heightmapViewer, this.voxelmapViewer);
         // this.terrainViewer.parameters.lod.enabled = false;
         this.terrainViewer.parameters.lod.wireframe = true;
+
         this.scene.add(this.terrainViewer.container);
 
         this.voxelmapVisibilityComputer = new VoxelmapVisibilityComputer(
