3dc99f9093f7bc997f078ba8591901852be15d39
Jérémie Piellard <jeremie.piellard@gmail.com>
2024-07-15 11:55:01 +0200

    feat: improve LOD meshing

--- src/lib/terrain/heightmap/heightmap-node-geometry.ts
+++ src/lib/terrain/heightmap/heightmap-node-geometry.ts
@@ -1,13 +1,13 @@
-enum EEdgeTesselation {
+enum EEdgeResolution {
     SIMPLE = 0,
-    TESSELATED = 1,
+    DECIMATED = 1,
 }
 
-type EdgesTesselation = {
-    up: EEdgeTesselation;
-    down: EEdgeTesselation;
-    left: EEdgeTesselation;
-    right: EEdgeTesselation;
+type EdgesResolution = {
+    up: EEdgeResolution;
+    down: EEdgeResolution;
+    left: EEdgeResolution;
+    right: EEdgeResolution;
 };
 
 type Indices = {
@@ -35,19 +35,19 @@ class HeightmapNodeGeometry {
         this.quadsCount = quadsCount;
 
         const positions: number[] = [];
-        for (let i = 0; i <= quadsCount; i += 0.5) {
+        for (let i = 0; i <= quadsCount; i++) {
             // top edge
             positions.push(i, 0, quadsCount);
         }
-        for (let i = quadsCount - 0.5; i >= 0; i -= 0.5) {
+        for (let i = quadsCount - 1; i >= 0; i--) {
             // right edge
             positions.push(quadsCount, 0, i);
         }
-        for (let i = quadsCount - 0.5; i >= 0; i -= 0.5) {
+        for (let i = quadsCount - 1; i >= 0; i--) {
             // bottom edge
             positions.push(i, 0, 0);
         }
-        for (let i = 0.5; i < quadsCount; i += 0.5) {
+        for (let i = 1; i < quadsCount; i++) {
             // left edge
             positions.push(0, 0, i);
         }
@@ -63,13 +63,13 @@ class HeightmapNodeGeometry {
         return new Float32Array(this.positionsBuffer);
     }
 
-    public getIndices(edgesTesselation: EdgesTesselation): Indices {
-        const cacheKey = this.buildEdgesCode(edgesTesselation);
+    public getIndices(edgesResolution: EdgesResolution): Indices {
+        const cacheKey = this.buildEdgesCode(edgesResolution);
         let result = this.indexBuffers[cacheKey];
         if (!result) {
             const indexData: number[] = [];
 
-            const buildInnerIndex = (x: number, y: number) => 4 * 2 * this.quadsCount + x + y * (this.quadsCount - 1);
+            const buildInnerIndex = (x: number, y: number) => 4 * this.quadsCount + x + y * (this.quadsCount - 1);
             for (let iX = 0; iX < this.quadsCount - 2; iX++) {
                 for (let iY = 0; iY < this.quadsCount - 2; iY++) {
                     const mm = buildInnerIndex(iX + 0, iY + 0);
@@ -81,39 +81,43 @@ class HeightmapNodeGeometry {
             }
 
             const buildEdge = (
-                edgeTesselation: EEdgeTesselation,
+                edgeResolution: EEdgeResolution,
                 edgeIndexFrom: number,
                 innerIndexFrom: number,
                 innerIndexStep: number,
                 invert: boolean
             ) => {
-                if (edgeTesselation === EEdgeTesselation.TESSELATED) {
-                    for (let iEdge = 0; iEdge < 2 * this.quadsCount; iEdge += 2) {
+                if (edgeResolution === EEdgeResolution.DECIMATED) {
+                    for (let iEdge = 0; iEdge < this.quadsCount; iEdge += 2) {
                         const iEdgeIndex = edgeIndexFrom + iEdge;
                         const e1 = iEdgeIndex;
-                        const e2 = iEdgeIndex + 1;
-                        const e3 = (iEdgeIndex + 2) % (8 * this.quadsCount);
+                        const e2 = (iEdgeIndex + 2) % (4 * this.quadsCount);
 
-                        if (iEdge === 0 || iEdge === 2 * this.quadsCount - 2) {
-                            const i1 = iEdge === 0 ? innerIndexFrom : innerIndexFrom + (this.quadsCount - 2) * innerIndexStep;
-                            indexData.push(e1, e2, i1, e2, e3, i1);
+                        if (iEdge === 0) {
+                            const i1 = innerIndexFrom;
+                            indexData.push(e1, e2, i1);
                         } else {
-                            const i1 = innerIndexFrom + (iEdge / 2 - 1) * innerIndexStep;
+                            const i1 = innerIndexFrom + (iEdge - 2) * innerIndexStep;
                             const i2 = i1 + innerIndexStep;
-                            indexData.push(i1, e1, e2, i1, e2, i2, e2, e3, i2);
+                            const i3 = i2 + innerIndexStep;
+                            indexData.push(i1, e1, i2, i2, e1, i3);
+                            indexData.push(e1, e2, i3);
                         }
                     }
                 } else {
-                    for (let iEdge = 0; iEdge < 2 * this.quadsCount; iEdge += 2) {
+                    for (let iEdge = 0; iEdge < this.quadsCount; iEdge++) {
                         const iEdgeIndex = edgeIndexFrom + iEdge;
                         const e1 = iEdgeIndex;
-                        const e2 = (iEdgeIndex + 2) % (8 * this.quadsCount);
+                        const e2 = (iEdgeIndex + 1) % (4 * this.quadsCount);
 
-                        if (iEdge === 0 || iEdge === 2 * this.quadsCount - 2) {
-                            const i1 = iEdge === 0 ? innerIndexFrom : innerIndexFrom + (this.quadsCount - 2) * innerIndexStep;
+                        if (iEdge === 0) {
+                            const i1 = innerIndexFrom;
+                            indexData.push(e1, e2, i1);
+                        } else if (iEdge === this.quadsCount - 1) {
+                            const i1 = innerIndexFrom + (this.quadsCount - 2) * innerIndexStep;
                             indexData.push(e1, e2, i1);
                         } else {
-                            const i1 = innerIndexFrom + (iEdge / 2) * innerIndexStep;
+                            const i1 = innerIndexFrom + iEdge * innerIndexStep;
                             const i2 = i1 - innerIndexStep;
 
                             if (invert) {
@@ -126,28 +130,28 @@ class HeightmapNodeGeometry {
                 }
 
                 const edgeIndices: number[] = [];
-                for (let iEdge = 0; iEdge <= 2 * this.quadsCount; iEdge++) {
-                    const iEdgeIndex = (edgeIndexFrom + iEdge) % (8 * this.quadsCount);
+                for (let iEdge = 0; iEdge <= this.quadsCount; iEdge++) {
+                    const iEdgeIndex = (edgeIndexFrom + iEdge) % (4 * this.quadsCount);
                     edgeIndices.push(iEdgeIndex);
                 }
                 return edgeIndices;
             };
 
-            const mpIndex = 0 * (2 * this.quadsCount);
-            const ppIndex = 1 * (2 * this.quadsCount);
-            const pmIndex = 2 * (2 * this.quadsCount);
-            const mmIndex = 3 * (2 * this.quadsCount);
+            const mpIndex = 0 * this.quadsCount;
+            const ppIndex = 1 * this.quadsCount;
+            const pmIndex = 2 * this.quadsCount;
+            const mmIndex = 3 * this.quadsCount;
 
-            const up = buildEdge(edgesTesselation.up, mpIndex, buildInnerIndex(0, this.quadsCount - 2), 1, true);
+            const up = buildEdge(edgesResolution.up, mpIndex, buildInnerIndex(0, this.quadsCount - 2), 1, true);
             const right = buildEdge(
-                edgesTesselation.right,
+                edgesResolution.right,
                 ppIndex,
                 buildInnerIndex(this.quadsCount - 2, this.quadsCount - 2),
                 -(this.quadsCount - 1),
                 false
             );
-            const down = buildEdge(edgesTesselation.down, pmIndex, buildInnerIndex(this.quadsCount - 2, 0), -1, true);
-            const left = buildEdge(edgesTesselation.left, mmIndex, buildInnerIndex(0, 0), this.quadsCount - 1, false);
+            const down = buildEdge(edgesResolution.down, pmIndex, buildInnerIndex(this.quadsCount - 2, 0), -1, true);
+            const left = buildEdge(edgesResolution.left, mmIndex, buildInnerIndex(0, 0), this.quadsCount - 1, false);
 
             result = {
                 buffer: indexData.slice(0),
@@ -169,9 +173,9 @@ class HeightmapNodeGeometry {
         };
     }
 
-    private buildEdgesCode(edgesTesselation: EdgesTesselation): string {
-        return `${edgesTesselation.up}_${edgesTesselation.down}_${edgesTesselation.left}_${edgesTesselation.right}`;
+    private buildEdgesCode(edgesResolution: EdgesResolution): string {
+        return `${edgesResolution.up}_${edgesResolution.down}_${edgesResolution.left}_${edgesResolution.right}`;
     }
 }
 
-export { EEdgeTesselation, HeightmapNodeGeometry, type EdgesTesselation, type Indices };
+export { EEdgeResolution, HeightmapNodeGeometry, type EdgesResolution, type Indices };
