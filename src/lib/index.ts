27f404188d56af1b29ae2348721ebdecb7adf998
Jérémie Piellard <jeremie.piellard@gmail.com>
2024-07-28 16:23:05 +0200

    build: expose HeightmapViewer

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 export { PromisesQueue } from './helpers/async/promises-queue';
 export { ELogLevel, setVerbosity } from './helpers/logger';
+export { HeightmapViewer } from './terrain/heightmap/heightmap-viewer';
 export type { IHeightmap, IHeightmapCoords, IHeightmapSample } from './terrain/heightmap/i-heightmap';
 export { TerrainViewer } from './terrain/terrain-viewer';
 export type { ILocalMapData, IVoxelMap, IVoxelMaterial, VoxelsChunkSize } from './terrain/voxelmap/i-voxelmap';
