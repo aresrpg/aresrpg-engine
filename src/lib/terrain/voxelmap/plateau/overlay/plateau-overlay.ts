049cad839145213f0066dcd4e65dd393b928a6cf
Jérémie Piellard <jeremie.piellard@gmail.com>
2024-08-13 17:19:41 +0200

    refactor: PlateauOverlay, use XZ coordinates

--- src/lib/terrain/voxelmap/plateau/overlay/plateau-overlay.ts
+++ src/lib/terrain/voxelmap/plateau/overlay/plateau-overlay.ts
@@ -1,9 +1,10 @@
-import { vec2ToString } from '../../../../helpers/string';
 import * as THREE from '../../../../three-usage';
+
+type GridCoord = { readonly x: number, readonly z: number };
 
 type Parameters = {
     readonly name: string;
-    readonly size: THREE.Vector2Like;
+    readonly size: GridCoord;
     readonly uniforms: Record<string, THREE.IUniform<unknown>>;
     readonly fragmentShader: string;
 };
@@ -11,7 +12,7 @@ type Parameters = {
 abstract class PlateauOverlay {
     public readonly container: THREE.Object3D;
 
-    private readonly gridSize: THREE.Vector2Like;
+    private readonly gridSize: GridCoord;
 
     private readonly texture: THREE.DataTexture;
     private readonly textureData: Uint8Array;
@@ -22,8 +23,8 @@ abstract class PlateauOverlay {
     protected constructor(params: Parameters) {
         this.gridSize = params.size;
 
-        this.textureData = new Uint8Array(4 * this.gridSize.x * this.gridSize.y);
-        this.texture = new THREE.DataTexture(this.textureData, this.gridSize.x, this.gridSize.y);
+        this.textureData = new Uint8Array(4 * this.gridSize.x * this.gridSize.z);
+        this.texture = new THREE.DataTexture(this.textureData, this.gridSize.x, this.gridSize.z);
 
         this.material = new THREE.ShaderMaterial({
             glslVersion: '300 es',
@@ -44,7 +45,7 @@ void main(void) {
                 0,
                 this.gridSize.x,
                 0,
-                this.gridSize.y,
+                this.gridSize.z,
                 this.gridSize.x,
                 0,
                 0,
@@ -53,10 +54,10 @@ void main(void) {
                 0,
                 0,
                 0,
-                this.gridSize.y,
+                this.gridSize.z,
                 this.gridSize.x,
                 0,
-                this.gridSize.y,
+                this.gridSize.z,
             ],
             3
         );
@@ -79,15 +80,16 @@ void main(void) {
         this.material.dispose();
     }
 
-    protected setTexel(position: THREE.Vector2Like, texelData: [number, number, number, number]): void {
-        if (position.x < 0 || position.y < 0 || position.x >= this.gridSize.x || position.y >= this.gridSize.y) {
-            throw new Error(`Out of bounds position "${vec2ToString(position)}" (size is "${vec2ToString(this.gridSize)}")`);
+    protected setTexel(position: GridCoord, texelData: [number, number, number, number]): void {
+        if (position.x < 0 || position.z < 0 || position.x >= this.gridSize.x || position.z >= this.gridSize.z) {
+            throw new Error(`Out of bounds position "${position.x}x${position.z}" (size is "${this.gridSize.x}x${this.gridSize.z}")`);
         }
 
-        const index = 4 * (position.x + position.y * this.gridSize.x);
+        const index = 4 * (position.x + position.z * this.gridSize.x);
         this.textureData.set(texelData, index);
         this.texture.needsUpdate = true;
     }
 }
 
-export { PlateauOverlay };
+export { PlateauOverlay, type GridCoord };
+
