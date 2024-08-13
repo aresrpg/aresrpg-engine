bdadeb041e793b22299b1bf621afaeea0f0368d6
Jérémie Piellard <jeremie.piellard@gmail.com>
2024-08-13 16:39:12 +0200

    feat: PlateauOverlay classes to highlight parts of the plateau

---
+++ src/lib/terrain/voxelmap/plateau/overlay/plateau-overlay-squares.ts
@@ -0,0 +1,66 @@
+import type * as THREE from '../../../../three-usage';
+
+import { PlateauOverlay } from './plateau-overlay';
+
+type Parameters = {
+    readonly size: THREE.Vector2Like;
+    readonly color?: THREE.Color;
+    readonly margin?: number;
+    readonly innerCornerRadius?: number;
+};
+
+class PlateauOverlaySquares extends PlateauOverlay {
+    public constructor(params: Parameters) {
+        const marginUniform = { value: params.margin ?? 0.05 };
+        const innerCornerRadiusUniform = { value: params.innerCornerRadius ?? 0.2 };
+
+        super({
+            name: 'blob',
+            size: params.size,
+            uniforms: {
+                uMargin: marginUniform,
+                uInnerCornerRadius: innerCornerRadiusUniform,
+            },
+            fragmentShader: `uniform sampler2D uDataTexture;
+uniform float uMargin;
+uniform float uInnerCornerRadius;
+
+in vec2 vGridCell;
+out vec4 fragColor;
+
+void main(void) {
+    ivec2 gridCellId = ivec2(floor(vGridCell));
+    vec2 uv = fract(vGridCell);
+
+    vec4 data = texelFetch(uDataTexture, gridCellId, 0);
+    if (data.a == 0.0) {
+        discard;
+    }
+
+    float border = 0.0;
+    float r2 = 0.5 - uMargin;
+    float r1 = r2 - border - uInnerCornerRadius;
+
+    vec2 dCenter = max(vec2(0), abs(uv - 0.5) - r1);
+    float dist = length(dCenter);
+
+    float alpha = step(dist, r2 - r1);
+
+    if (alpha == 0.0) {
+        discard;
+    }
+    fragColor = vec4(data.rgb, alpha);
+}`,
+        });
+    }
+
+    public enableCell(cellId: THREE.Vector2Like, color: THREE.Color): void {
+        this.setTexel(cellId, [Math.floor(255 * color.r), Math.floor(255 * color.g), Math.floor(255 * color.b), 255]);
+    }
+
+    public disableCell(cellId: THREE.Vector2Like): void {
+        this.setTexel(cellId, [0, 0, 0, 0]);
+    }
+}
+
+export { PlateauOverlaySquares };
---
+++ src/lib/terrain/voxelmap/plateau/overlay/plateau-overlay.ts
@@ -0,0 +1,85 @@
+import { vec2ToString } from '../../../../helpers/string';
+import * as THREE from '../../../../three-usage';
+
+type Parameters = {
+    readonly name: string;
+    readonly size: THREE.Vector2Like;
+    readonly uniforms: Record<string, THREE.IUniform<unknown>>;
+    readonly fragmentShader: string;
+};
+
+abstract class PlateauOverlay {
+    public readonly container: THREE.Object3D;
+
+    private readonly gridSize: THREE.Vector2Like;
+
+    private readonly texture: THREE.DataTexture;
+    private readonly textureData: Uint8Array;
+
+    protected constructor(params: Parameters) {
+        this.gridSize = params.size;
+
+        this.textureData = new Uint8Array(4 * this.gridSize.x * this.gridSize.y);
+        this.texture = new THREE.DataTexture(this.textureData, this.gridSize.x, this.gridSize.y);
+
+        const material = new THREE.ShaderMaterial({
+            glslVersion: '300 es',
+            transparent: true,
+            uniforms: { ...params.uniforms, uDataTexture: { value: this.texture } },
+            vertexShader: `out vec2 vGridCell;
+void main(void) {
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vGridCell = position.xz;
+}`,
+            fragmentShader: params.fragmentShader,
+        });
+
+        const positionsAttribute = new THREE.Float32BufferAttribute(
+            [
+                0,
+                0,
+                0,
+                this.gridSize.x,
+                0,
+                this.gridSize.y,
+                this.gridSize.x,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                this.gridSize.y,
+                this.gridSize.x,
+                0,
+                this.gridSize.y,
+            ],
+            3
+        );
+        const geometry = new THREE.BufferGeometry();
+        geometry.setAttribute('position', positionsAttribute);
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.frustumCulled = false;
+        this.container = new THREE.Group();
+        this.container.name = `plateau-overlay ${params.name}`;
+        this.container.add(mesh);
+    }
+
+    public clear(): void {
+        this.textureData.fill(0);
+        this.texture.needsUpdate = true;
+    }
+
+    protected setTexel(position: THREE.Vector2Like, texelData: [number, number, number, number]): void {
+        if (position.x < 0 || position.y < 0 || position.x >= this.gridSize.x || position.y >= this.gridSize.y) {
+            throw new Error(`Out of bounds position "${vec2ToString(position)}" (size is "${vec2ToString(this.gridSize)}")`);
+        }
+
+        const index = 4 * (position.x + position.y * this.gridSize.x);
+        this.textureData.set(texelData, index);
+        this.texture.needsUpdate = true;
+    }
+}
+
+export { PlateauOverlay };
