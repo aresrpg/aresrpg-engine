6382f1a54666d49a4e321656abcf1da2c113007d
Jérémie Piellard <jeremie.piellard@gmail.com>
2024-09-05 14:07:12 +0200

    style: linting

--- src/test/webpack.config.js
+++ src/test/webpack.config.js
@@ -2,7 +2,13 @@ const path = require('path');
 
 const PROJECT_DIR = path.resolve(__dirname, '..', '..');
 
-function makeExport(sourceFilepath /* string */, outFolder /* string */, mode /* string */, library /* optional string */, externals /* object */) /* void */ {
+function makeExport(
+  sourceFilepath /* string */,
+  outFolder /* string */,
+  mode /* string */,
+  library /* optional string */,
+  externals /* object */
+) /* void */ {
   const name = library ? library.toLowerCase() : '[name]';
   const filename = `${name}${mode === 'production' ? '.min' : ''}.js`;
 
@@ -51,8 +57,8 @@ const targetDir = path.join(PROJECT_DIR, 'test', 'script');
 
 module.exports = [
   makeExport(path.join(srcDir, 'main.ts'), targetDir, 'development', undefined, {
-    "three-usage-test": "THREE",
-    "three": "THREE",
+    'three-usage-test': 'THREE',
+    three: 'THREE',
   }),
-  makeExport(path.join(srcDir, "libs", "three-usage-test.ts"), targetDir, 'development', "THREE"),
+  makeExport(path.join(srcDir, 'libs', 'three-usage-test.ts'), targetDir, 'development', 'THREE'),
 ];
