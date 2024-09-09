9a6cb9abee9733c7032d6b456aad1a1084a59ba2
Jérémie Piellard <jeremie.piellard@gmail.com>
2024-09-09 18:30:33 +0200

    feat: add support for infinite number of billboard instances

--- src/lib/effects/billboard/instanced-billboard.ts
+++ src/lib/effects/billboard/instanced-billboard.ts
@@ -7,22 +7,22 @@ type Parameters = {
     readonly origin: THREE.Vector2Like;
     readonly lockAxis?: THREE.Vector3Like;
     readonly baseSize: THREE.Vector2Like;
-    readonly maxInstancesCount: number;
 };
 
 class InstancedBillboard {
     public readonly container: THREE.Object3D;
 
-    private readonly mesh: THREE.InstancedMesh;
-    public readonly maxInstancesCount: number;
+    private readonly billboardGeometry: THREE.BufferGeometry;
+    private readonly billboardMaterial: THREE.Material;
+    private readonly meshes: THREE.InstancedMesh[] = [];
+    private readonly maxInstancesPerMesh = 2000;
 
     public constructor(params: Parameters) {
         this.container = new THREE.Group();
-        this.maxInstancesCount = params.maxInstancesCount;
 
         const spritesheet: Spritesheet = {
-            texture: new THREE.TextureLoader().load('/resources/puff.png'),
-            size: { x: 3, y: 4 },
+            texture: new THREE.TextureLoader().load('/resources/tree.png'),
+            size: { x: 1, y: 1 },
         };
 
         const lifetimeUniform: THREE.IUniform<number> = { value: 0 };
@@ -30,23 +30,24 @@ class InstancedBillboard {
             lifetimeUniform.value = performance.now() / 100;
         }, 50);
 
-        const bufferGeometry = new THREE.BufferGeometry();
-        bufferGeometry.setAttribute(
+        this.billboardGeometry = new THREE.BufferGeometry();
+        this.billboardGeometry.setAttribute(
             'position',
             new THREE.Float32BufferAttribute([0.5, 0.5, 0, -0.5, 0.5, 0, -0.5, -0.5, 0, 0.5, 0.5, 0, -0.5, -0.5, 0, 0.5, -0.5, 0], 3)
         );
-        bufferGeometry.setAttribute('normal', new THREE.Float32BufferAttribute([0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1], 3));
-        bufferGeometry.setAttribute('uv', new THREE.Float32BufferAttribute([0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1], 2));
+        this.billboardGeometry.setAttribute(
+            'normal',
+            new THREE.Float32BufferAttribute([0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1], 3)
+        );
+        this.billboardGeometry.setAttribute('uv', new THREE.Float32BufferAttribute([0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1], 2));
 
-        this.mesh = new THREE.InstancedMesh(
-            bufferGeometry,
-            new THREE.ShaderMaterial({
-                uniforms: {
-                    uSpritesheetTexture: { value: spritesheet.texture },
-                    uSpritesheetSize: { value: spritesheet.size },
-                    uLifetime: lifetimeUniform,
-                },
-                vertexShader: `
+        this.billboardMaterial = new THREE.ShaderMaterial({
+            uniforms: {
+                uSpritesheetTexture: { value: spritesheet.texture },
+                uSpritesheetSize: { value: spritesheet.size },
+                uLifetime: lifetimeUniform,
+            },
+            vertexShader: `
 uniform float uLifetime;
 uniform vec2 uSpritesheetSize;
 
@@ -63,9 +64,6 @@ void main() {
     const vec2 size = vec2(${params.baseSize.x}, ${params.baseSize.y});
     const vec3 origin = vec3(${params.origin.x.toFixed(3)}, ${params.origin.y.toFixed(3)}, 0);
 
-    // vec4 instanceOrigin4 = instanceMatrix * vec4(origin, 1);
-    // vec3 instanceOrigin = instanceOrigin4.xyz / instanceOrigin4.w;
-
     vec3 modelPosition = origin +
         size.x * position.x * right +
         size.y * position.y * up;
@@ -74,13 +72,13 @@ void main() {
     vUv = uv;
 
     float spriteId = 3.0;//mod(uLifetime, uSpritesheetSize.x * uSpritesheetSize.y);
-    vSpriteId = floor(vec2(
-        mod(spriteId, uSpritesheetSize.x),
-        spriteId / uSpritesheetSize.x
-    ));
+    vSpriteId = vec2(0,0);//floor(vec2(
+    //     mod(spriteId, uSpritesheetSize.x),
+    //     spriteId / uSpritesheetSize.x
+    // ));
 }
-            `,
-                fragmentShader: `
+        `,
+            fragmentShader: `
 uniform sampler2D uSpritesheetTexture;
 uniform vec2 uSpritesheetSize;
 
@@ -95,23 +93,36 @@ vec4 sampleTexture() {
 void main() {
     vec4 sampled = sampleTexture();
     if (sampled.a < 0.5) {
-        // discard;
+        discard;
     }
-    // sampled.rgb /= sampled.a;
+    sampled.rgb /= sampled.a;
 
-    gl_FragColor = vec4(vUv, 0, 1) + 0.000001 * vec4(sampled.rgb, 1);
+    gl_FragColor = vec4(sampled.rgb, 1);
 }
 `,
-                side: THREE.DoubleSide,
-            }),
-            params.maxInstancesCount
-        );
-        this.mesh.frustumCulled = false;
-        this.container.add(this.mesh);
+            side: THREE.DoubleSide,
+        });
     }
 
     public setInstancesCount(value: number): void {
-        this.mesh.count = value;
+        const requiredMeshesCount = Math.ceil(value / this.maxInstancesPerMesh);
+        while (this.meshes.length < requiredMeshesCount) {
+            const mesh = new THREE.InstancedMesh(this.billboardGeometry, this.billboardMaterial, this.maxInstancesPerMesh);
+            mesh.count = 0;
+            mesh.frustumCulled = false;
+            this.container.add(mesh);
+            this.meshes.push(mesh);
+        }
+
+        this.meshes.forEach((mesh: THREE.InstancedMesh, index: number) => {
+            if (value < index * this.maxInstancesPerMesh) {
+                mesh.count = 0;
+            } else if ((index + 1) * this.maxInstancesPerMesh <= value) {
+                mesh.count = this.maxInstancesPerMesh;
+            } else {
+                mesh.count = value - index * this.maxInstancesPerMesh;
+            }
+        });
     }
 
     public setInstanceTransform(instanceId: number, position: THREE.Vector3Like, rotation: number, scaling: number): void {
@@ -122,8 +133,13 @@ void main() {
                 new THREE.Matrix4().makeScale(scaling, scaling, scaling)
             )
         );
-        this.mesh.setMatrixAt(instanceId, matrix);
-        this.mesh.instanceMatrix.needsUpdate = true;
+
+        const mesh = this.meshes[Math.floor(instanceId / this.maxInstancesPerMesh)];
+        if (!mesh) {
+            throw new Error('No mesh');
+        }
+        mesh.setMatrixAt(instanceId % this.maxInstancesPerMesh, matrix);
+        mesh.instanceMatrix.needsUpdate = true;
     }
 }
 
