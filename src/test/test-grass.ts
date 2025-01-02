4db53584b1b896ea220818b1d0764ea6425f6c98
Jérémie Piellard <jeremie.piellard@gmail.com>
2025-01-02 19:44:34 +0100

    test: dynamic & infinite grass generation

--- src/test/test-grass.ts
+++ src/test/test-grass.ts
@@ -59,9 +59,8 @@ enum EGrassMode {
     GRASS_3D = '3d',
 }
 
-type PositionsList = { position: THREE.Vector2Like }[];
 interface IRepartition {
-    getAllItems(from: THREE.Vector2Like, to: THREE.Vector2Like): PositionsList;
+    getAllItems(from: THREE.Vector2Like, to: THREE.Vector2Like): THREE.Vector2Like[];
 }
 
 type Parameters = {
@@ -83,7 +82,10 @@ class TestGrass extends TestBase {
     private readonly grass3D: PropsHandler;
     private readonly rocks: PropsHandler;
 
-    private readonly fakeCamera: THREE.Object3D;
+    readonly grassRepartition: IRepartition;
+    readonly rocksRepartition: IRepartition;
+
+    private readonly fakePlayer: THREE.Object3D;
 
     private readonly parameters = {
         viewRadius: 20,
@@ -97,18 +99,16 @@ class TestGrass extends TestBase {
 
         const whitenoiseDensity = 0.5;
         const whitenoiseRepartition: IRepartition = {
-            getAllItems(from: THREE.Vector2Like, to: THREE.Vector2Like): PositionsList {
-                const result: PositionsList = [];
+            getAllItems(from: THREE.Vector2Like, to: THREE.Vector2Like): THREE.Vector2Like[] {
+                const result: THREE.Vector2Like[] = [];
                 const areaSize = new THREE.Vector2().subVectors(to, from);
                 const totalArea = areaSize.x * areaSize.y;
                 const totalItemsCount = totalArea * whitenoiseDensity;
 
                 for (let i = 0; i < totalItemsCount; i++) {
                     result.push({
-                        position: {
-                            x: from.x + areaSize.x * Math.random(),
-                            y: from.y + areaSize.y * Math.random(),
-                        },
+                        x: from.x + areaSize.x * Math.random(),
+                        y: from.y + areaSize.y * Math.random(),
                     });
                 }
 
@@ -120,16 +120,12 @@ class TestGrass extends TestBase {
         const bluenoiseDensity = 2;
         const bluenoiseScaling = Math.ceil(bluenoiseDensity / 0.6);
         const bluenoiseRepartition: IRepartition = {
-            getAllItems(from: THREE.Vector2Like, to: THREE.Vector2Like): PositionsList {
+            getAllItems(from: THREE.Vector2Like, to: THREE.Vector2Like): THREE.Vector2Like[] {
                 const rawItems = repeatableBluenoise.getAllItems(
                     new THREE.Vector2().addScaledVector(from, bluenoiseScaling),
                     new THREE.Vector2().addScaledVector(to, bluenoiseScaling)
                 );
-                return rawItems.map(item => {
-                    return {
-                        position: new THREE.Vector2().addScaledVector(item.position, 1 / bluenoiseScaling),
-                    };
-                });
+                return rawItems.map(item => new THREE.Vector2().addScaledVector(item.position, 1 / bluenoiseScaling));
             },
         };
 
@@ -159,7 +155,9 @@ class TestGrass extends TestBase {
         const propsContainer = new THREE.Object3D();
         this.scene.add(propsContainer);
 
-        const allGrassParticlesPositions = params.repartitions.bluenoise.getAllItems({ x: -100, y: -100 }, { x: 100, y: 100 });
+        this.grassRepartition = params.repartitions.bluenoise;
+        this.rocksRepartition = params.repartitions.whitenoise;
+
         this.grass2D = new PropsHandler({
             bufferGeometry: params.propDefinitions.grass2D.bufferGeometry,
             material: params.propDefinitions.grass2D.material,
@@ -172,44 +170,28 @@ class TestGrass extends TestBase {
             reactToPlayer: true,
         });
         propsContainer.add(this.grass3D.container);
-        const grassParticlesMatrices = allGrassParticlesPositions.map(particle =>
-            new THREE.Matrix4().multiplyMatrices(
-                new THREE.Matrix4().makeTranslation(new THREE.Vector3(particle.position.x, 0, particle.position.y)),
-                new THREE.Matrix4().makeRotationY((Math.PI / 2) * Math.random()) // Math.floor(4 * Math.random())),
-            )
-        );
-        this.grass2D.setGroup('haha', grassParticlesMatrices);
-        this.grass3D.setGroup('haha', grassParticlesMatrices);
 
-        const allRockParticlesPositions = params.repartitions.whitenoise.getAllItems({ x: -100, y: -100 }, { x: 100, y: 100 });
         this.rocks = new PropsHandler({
             bufferGeometry: params.propDefinitions.rocks.bufferGeometry,
             material: params.propDefinitions.rocks.material,
             reactToPlayer: false,
         });
         propsContainer.add(this.rocks.container);
-        const rockParticlesMatrices = allRockParticlesPositions.map(particle =>
-            new THREE.Matrix4().multiplyMatrices(
-                new THREE.Matrix4().makeTranslation(new THREE.Vector3(particle.position.x, 0, particle.position.y)),
-                new THREE.Matrix4().makeRotationY((Math.PI / 2) * Math.random())
-            )
-        );
-        this.rocks.setGroup('haha', rockParticlesMatrices);
 
-        this.fakeCamera = new THREE.Object3D();
-        this.fakeCamera.position.set(0, 0.5, 0);
+        this.fakePlayer = new THREE.Object3D();
+        this.fakePlayer.position.set(0, 0.5, 0);
         const boardCenterControls = new THREE.TransformControls(this.camera, this.renderer.domElement);
         boardCenterControls.showY = false;
         boardCenterControls.addEventListener('dragging-changed', event => {
             this.cameraControl.enabled = !event.value;
         });
-        const fakePlayer = new THREE.Mesh(
+        const fakePlayerMesh = new THREE.Mesh(
             new THREE.SphereGeometry(0.5, 12, 12),
             new THREE.MeshPhongMaterial({ color: 0xffffff, wireframe: false })
         );
-        this.fakeCamera.add(fakePlayer);
-        boardCenterControls.attach(this.fakeCamera);
-        this.scene.add(this.fakeCamera);
+        this.fakePlayer.add(fakePlayerMesh);
+        boardCenterControls.attach(this.fakePlayer);
+        this.scene.add(this.fakePlayer);
         this.scene.add(boardCenterControls.getHelper());
 
         const groundSize = 1000;
@@ -258,16 +240,55 @@ class TestGrass extends TestBase {
 
         this.gui = new GUI();
         this.gui.show();
-        this.gui.add(this.parameters, 'viewRadius', 0, 200, 1).name('View distance').onChange(applyViewDistance);
+        this.gui.add(this.parameters, 'viewRadius', 0, 1000, 1).name('View distance').onChange(applyViewDistance);
         this.gui.add(this.parameters, 'viewRadiusMargin', 0, 50, 0.1).name('View distance margin').onChange(applyViewDistanceMargin);
-        this.gui.add(fakePlayer, 'visible').name('Show player');
+        this.gui.add(fakePlayerMesh, 'visible').name('Show player');
         this.gui.add(ground, 'visible').name('Show ground');
         this.gui.add(propsContainer, 'visible').name('Show props');
         this.gui.add(this.parameters, 'grassMode', Object.values(EGrassMode)).name('Grass type').onChange(applyGrassMode);
     }
 
     protected override update(): void {
-        const playerViewPosition = this.fakeCamera.getWorldPosition(new THREE.Vector3()).applyMatrix4(this.camera.matrixWorldInverse);
+        const cameraWorldPosition3d = this.camera.getWorldPosition(new THREE.Vector3());
+
+        const patchSize = 64;
+        const fromPatch = new THREE.Vector2(cameraWorldPosition3d.x, cameraWorldPosition3d.z).subScalar(this.parameters.viewRadius).divideScalar(patchSize).floor();
+        const toPatch = new THREE.Vector2(cameraWorldPosition3d.x, cameraWorldPosition3d.z).addScalar(this.parameters.viewRadius).divideScalar(patchSize).floor();
+
+        for (let iX = fromPatch.x; iX <= toPatch.x; iX++) {
+            for (let iY = fromPatch.y; iY <= toPatch.y; iY++) {
+                if ((iX + iY) % 2 === 0) {
+                    continue;
+                }
+
+                const patchId = `${iX}_${iY}`;
+                if (!this.grass2D.hasGroup(patchId)) {
+                    const patchStart = new THREE.Vector2(iX, iY).multiplyScalar(patchSize);
+                    const patchEnd = new THREE.Vector2(iX, iY).addScalar(1).multiplyScalar(patchSize);
+
+                    const grassParticlesPositions = this.grassRepartition.getAllItems(patchStart, patchEnd);
+                    const grassParticlesMatrices = grassParticlesPositions.map(position =>
+                        new THREE.Matrix4().multiplyMatrices(
+                            new THREE.Matrix4().makeTranslation(new THREE.Vector3(position.x, 0, position.y)),
+                            new THREE.Matrix4().makeRotationY((Math.PI / 2) * Math.random()) // Math.floor(4 * Math.random())),
+                        )
+                    );
+                    this.grass2D.setGroup(patchId, grassParticlesMatrices);
+                    this.grass3D.setGroup(patchId, grassParticlesMatrices);
+
+                    const rockParticlesPositions = this.rocksRepartition.getAllItems(patchStart, patchEnd);
+                    const rockParticlesMatrices = rockParticlesPositions.map(position =>
+                        new THREE.Matrix4().multiplyMatrices(
+                            new THREE.Matrix4().makeTranslation(new THREE.Vector3(position.x, 0, position.y)),
+                            new THREE.Matrix4().makeRotationY((Math.PI / 2) * Math.random())
+                        )
+                    );
+                    this.rocks.setGroup(patchId, rockParticlesMatrices);
+                }
+            }
+        }
+
+        const playerViewPosition = this.fakePlayer.getWorldPosition(new THREE.Vector3()).applyMatrix4(this.camera.matrixWorldInverse);
 
         this.grass2D.setPlayerViewPosition(playerViewPosition);
         this.grass3D.setPlayerViewPosition(playerViewPosition);
